acd46902cd05260c10eddceaf30a3b73782ecb66
Dan Holtby <djholtby@uwaterloo.ca>
2020-11-13 12:22:12 -0500

    Add .gitignore, scribblings, and info settings

--- info.rkt
+++ info.rkt
@@ -1,5 +1,14 @@
 #lang info
 
+(define collection "uwaterloo-racket")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/uwaterloo-racket.scrbl" ())))
+(define pkg-desc "University of Waterloo Racket plugins and libraries")
+(define version "0.0")
+(define pkg-authors '(djholtby))
+
+
 (define drracket-tool-names (list "Graphic Blocker"))
 (define drracket-tools (list (list "graphic-block.rkt")))
 
---
+++ scribblings/uwaterloo-racket.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[uwaterloo-racket
+                    racket/base]]
+
+@title{UWaterloo Racket}
+@author{djholtby}
+
+@defmodule[uwaterloo-racket]
+
+Package Description Here
