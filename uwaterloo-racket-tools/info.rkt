15c2ec62af0de92d0dcb8fbd0976c79fa9e42444
djholtby <djholtby@gmail.com>
2020-11-15 10:21:58 -0500

    change metapackage description

--- uwaterloo-racket-tools/info.rkt
+++ uwaterloo-racket-tools/info.rkt
@@ -2,6 +2,7 @@
 (define collection "uwaterloo-racket-tools")
 (define deps '("htdp-trace" "graphic-block"))
 
-(define pkg-desc "Metapackage for tools and libraries used by UWaterloo's CS115 and CS135 courses")
+(define pkg-desc
+  "Metapackage for tools and libraries used by the University of Waterloo's first year courses")
 (define version "0.0")
 (define pkg-authors '(djholtby))
