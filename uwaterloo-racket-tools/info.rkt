e826cd19a6328a98273487c7fb2a3f0a656114e5
djholtby <djholtby@gmail.com>
2020-11-14 20:07:36 -0500

    create metapackage

---
+++ uwaterloo-racket-tools/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "uwaterloo-racket-tools")
+(define deps '("htdp-trace" "graphic-block"))
+
+(define pkg-desc "Metapackage for tools and libraries used by UWaterloo's CS115 and CS135 courses")
+(define version "0.0")
+(define pkg-authors '(djholtby))
