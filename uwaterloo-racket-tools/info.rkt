24f1c0034ea24180c4d501eb51efd96f5f349215
Dan Holtby <djholtby@uwaterloo.ca>
2020-11-17 12:38:23 -0500

    fix broken deps

--- uwaterloo-racket-tools/info.rkt
+++ uwaterloo-racket-tools/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "uwaterloo-racket-tools")
-(define deps '("htdp-trace" "graphic-block"))
+(define deps '("base" "htdp-trace" "graphic-block"))
 
 (define pkg-desc
   "Metapackage for tools and libraries used by the University of Waterloo's first year courses")
